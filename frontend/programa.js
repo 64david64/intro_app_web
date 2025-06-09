454778e4adc48ffbf4f94f09b8fd04f1c9c01552
Juan David Acosta <juandavidacosta1648@gmail.com>
2025-06-09 15:50:39 -0500

    v 0.3

--- frontend/programa.js
+++ frontend/programa.js
@@ -4,3 +4,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Manejar el evento de hacer click en el mapa
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        var marker = L.marker(e.latlng).addTo(map);
+}
+
+map.on('click', onMapClick);
